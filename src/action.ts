32322047598ff4cfe49bbe5a22120f0b599c24d6
erikdies <erik.dies@gmail.com>
2022-01-23 20:33:53 -0500

    add a log

--- src/action.ts
+++ src/action.ts
@@ -65,6 +65,7 @@ export async function getProjectData(
   options: GetProjectDataOptions
 ): Promise<ProjectData | undefined> {
   const {octokit, githubRepo, issueNumber, possible} = options;
+  core.info("Trying to get project data")
 
   const projects =
     (
